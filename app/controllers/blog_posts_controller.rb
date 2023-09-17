cba06f5718ca6d1d095b6fad16784bd77e85ec09
Krishna <libraryismyparadise@gmail.com>
2023-09-17 21:57:27 +0545

    Add minor refactor to routes and controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,13 +1,11 @@
 class BlogPostsController < ApplicationController
-    
+    before_action :set_blog_posts, except: [:index, :new, :create]
+
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-        @blog_post = BlogPost.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-        redirect_to root_path
     end
 
     def new 
@@ -24,11 +22,9 @@ class BlogPostsController < ApplicationController
 end
 
     def edit
-        @blog_post = BlogPost.find(params[:id])
     end
 
     def update
-        @blog_post = BlogPost.find(params[:id])
         if @blog_post.update(blog_post_params)
             redirect_to @blog_post
         else
@@ -37,7 +33,6 @@ end
     end
 
     def destroy
-        @blog_post = BlogPost.find(params[:id])
         @blog_post.destroy
         redirect_to root_path
     end
@@ -46,4 +41,11 @@ end
     def blog_post_params
         params.require(:blog_post).permit(:title, :body)
     end
+
+    def set_blog_posts
+        @blog_post = BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
+
+    end 
 end
