10f2bc081c736a8bd85fc6e5307570141b9c923e
Krishna <libraryismyparadise@gmail.com>
2025-05-01 17:43:12 +0900

    Refactor routes to streamline blog post resource definitions and maintain Devise authentication for Admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  delete "/blog_posts/:id", to: "blog_posts#destroy"
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
 
-  resources :blog_posts
-
+  resources :blog_posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   root "blog_posts#index"
 end
