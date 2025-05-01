3c340ed5259b7fa7dfc6dc517be963082fd4380b
Krishna <libraryismyparadise@gmail.com>
2025-05-01 18:36:30 +0900

    Remove commented-out devise modules from Admin model for cleaner code

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,5 @@
 class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :blog_posts, dependent: :destroy
 end
