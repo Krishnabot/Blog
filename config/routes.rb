38c3b2389860e6de01c4eeb6efa209c47277dd4e
Krishna <libraryismyparadise@gmail.com>
2025-05-01 18:30:47 +0900

    Refactor routes to organize public blog post access under readers namespace and update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins # Remove custom sessions controller for now
+  devise_for :admins
 
-  # Public routes
-  resources :blog_posts, only: [:index, :show]
+  # Public routes under readers namespace
+  namespace :readers do
+    resources :blog_posts, only: [:index, :show]
+  end
 
   # Admin routes
   namespace :admin do
     resources :blog_posts, only: [:new, :create, :edit, :update, :destroy]
   end
 
-  root "blog_posts#index"
+  # Root to public blog posts index
+  root "readers/blog_posts#index"
 end
