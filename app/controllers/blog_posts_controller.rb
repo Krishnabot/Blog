9e414ff2f55566226b3e0e46a09337a717779aae
Krishna <libraryismyparadise@gmail.com>
2025-05-01 18:18:09 +0900

    Refactor BlogPostsController to inherit from BaseController and streamline actions

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,51 +1,28 @@
-class BlogPostsController < ApplicationController
-    before_action :set_blog_posts, except: [:index, :new, :create]
+class BlogPostsController < BaseController
+  before_action :set_blog_post, only: [:show]
+  before_action :set_sidebar_data, only: [:index, :show]
 
-    def index
-        @blog_posts = BlogPost.all
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: 'Blog post not found.'
+  end
+
+  def set_sidebar_data
+    @posts_by_year = BlogPost.order(created_at: :desc).group_by { |post| post.created_at.year }
+    @posts_by_year.transform_values! do |posts|
+      posts.group_by { |post| post.created_at.strftime('%B') }.transform_values! do |month_posts|
+        month_posts.group_by { |p| p.created_at.strftime('Week %U') }
+      end
     end
-
-    def show
-    end
-
-    def new 
-        @blog_post = BlogPost.new
-    end
-
-    def create
-        @blog_post = BlogPost.new(blog_post_params)
-        if @blog_post.save
-            redirect_to @blog_post
-        else
-            render :new, status: :unprocessable_entity
-    end
-end
-
-    def edit
-    end
-
-    def update
-        if @blog_post.update(blog_post_params)
-            redirect_to @blog_post
-        else
-            render :edit, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @blog_post.destroy
-        redirect_to root_path
-    end
-
-    private
-    def blog_post_params
-        params.require(:blog_post).permit(:title, :body)
-    end
-
-    def set_blog_posts
-        @blog_post = BlogPost.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-        redirect_to root_path
-
-    end 
+  end
 end
