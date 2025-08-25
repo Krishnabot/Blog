c4705dbaa113c524b987b13d7942537049aeff2d
Krishna <libraryismyparadise@gmail.com>
2025-08-25 23:35:38 +0900

    Add jwt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_08_25_143408) do
+ActiveRecord::Schema[7.0].define(version: 2025_08_25_143504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,14 @@ ActiveRecord::Schema[7.0].define(version: 2025_08_25_143408) do
     t.string "tags", default: [], array: true
     t.index ["admin_id"], name: "index_blog_posts_on_admin_id"
     t.index ["slug"], name: "index_blog_posts_on_slug", unique: true
+  end
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.string "jti"
+    t.datetime "exp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
