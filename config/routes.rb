2ec302602fde34dd06e9649372a26244d37b9042
Krishna <libraryismyparadise@gmail.com>
2025-08-26 00:34:58 +0900

    update routes for admin and public API

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins,
-    path: "admins",
-    controllers: { sessions: "admins/sessions" }
-  namespace :admins do
-    get "dashboard", to: "dashboard#index"
-    resources :blog_posts, except: [:index, :show] 
-  end
-
+  devise_for :admins, path: "admins"
 
   namespace :api do
     namespace :v1 do
+      resources :articles, only: [:index, :show], param: :slug
       devise_scope :admin do
         post   "admin/sign_in",  to: "admins/sessions#create"
         delete "admin/sign_out", to: "admins/sessions#destroy"
       end
-
-      resources :articles, only: [:index, :show], param: :slug
-
       namespace :admin do
-        resources :articles, except: [:new, :edit], param: :id
+        resources :articles, except: [:new, :edit]
       end
     end
   end
